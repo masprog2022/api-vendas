b954697b534a9d0e5eef7f88b6c60a864bbb9d0d
mauroDevOps <mauro.dev09@gmail.com>
2022-04-22 13:00:13 +0100

    Bug Fixed 2

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -22,7 +22,7 @@ app.use(
     }
     return response.status(500).json({
       status: "error",
-      message: "Internal server error!!!",
+      message: "Internal server error",
     });
   }
 );
