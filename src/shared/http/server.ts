51b194e24ebfcadfd0e4e09d6749e261abc599bd
masprog2022 <masprog2022@gmail.com>
2022-04-26 23:43:44 +0100

    validacao dos dados da requisicao com celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import { errors } from "celebrate";
 import cors from "cors";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
