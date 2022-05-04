a7ab011bf2964773c03a47d7faa80754cc743fa3
masprog2022 <masprog2022@gmail.com>
2022-05-04 17:09:22 +0100

    rota de usuario

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
+import UsersController from "../controllers/UsersController";
+
+const userRouter = Router();
+const userController = new UsersController();
+
+userRouter.get("/", userController.index);
+userRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create
+);
+
+export default userRouter;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productRouter from "@modules/products/routes/products.routes";
+import userRouter from "@modules/users/routes/users.routes";
 
 const routes = Router();
 
 routes.use("/products", productRouter);
+routes.use("/users", userRouter);
 
 //routes.get("/", (request, response) => {
 //return response.json({ message: "Hello World!" });
