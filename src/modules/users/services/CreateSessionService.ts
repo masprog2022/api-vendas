01187492a202ffcb9351a2c482f5609e7708160e
masprog2022 <masprog2022@gmail.com>
2022-05-05 00:21:04 +0100

    autenticacao com token jwt

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,5 +1,6 @@
 import AppError from "@shared/errors/AppError";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
@@ -9,8 +10,13 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const user = await usersRepository.findByEmail(email);
@@ -25,7 +31,15 @@ class CreateSessionService {
       throw new AppError("Incorrect email/password combination. ", 401);
     }
 
-    return user;
+    const token = sign({}, "85228f55a22c857adb10ddb8dda020b0", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
