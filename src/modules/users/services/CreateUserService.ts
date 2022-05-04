14edb12f84288eb4b519ce1274a957bdb32ab98b
masprog2022 <masprog2022@gmail.com>
2022-05-04 18:00:56 +0100

    encriptar senha do user

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
@@ -19,10 +20,12 @@ class CreateUserService {
       throw new AppError("Email already used. ");
     }
 
+    const hashPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashPassword,
     });
 
     await usersRepository.save(user);
