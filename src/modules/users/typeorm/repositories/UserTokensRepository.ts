08b12ea16dfc809ce453083c9a49f58c3f40ba42
masprog2022 <masprog2022@gmail.com>
2022-05-06 17:26:49 +0100

    servico de envio de email para recuperacao de senha

--- src/modules/users/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from "typeorm";
 import UserToken from "../entities/UserToken";
 
 @EntityRepository(UserToken)
-class UserTokensRepository extends Repository<User> {
+class UserTokensRepository extends Repository<UserToken> {
   public async findByToken(token: string): Promise<UserToken | undefined> {
     const userToken = await this.findOne({
       where: {
