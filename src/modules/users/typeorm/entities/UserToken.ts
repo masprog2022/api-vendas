12895336677f99af2dadd1a13eeea2267c718fd0
masprog2022 <masprog2022@gmail.com>
2022-05-06 16:42:30 +0100

    entidade de tokens de usuarios

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  Entity,
+} from "typeorm";
+
+@Entity("users_tokens")
+class UserToken {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  @Generated("uuid")
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default UserToken;
