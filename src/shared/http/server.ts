5601e1f9c43c318cdf9ba9027cbc279e73e4a9d9
masprog2022 <masprog2022@gmail.com>
2022-05-27 09:40:28 +0100

    pagination de dados Lib TypeORM Pagination

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { errors } from "celebrate";
+import { pagination } from "typeorm-pagination";
 import cors from "cors";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
@@ -12,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(pagination);
 app.use("/files", express.static(uploadConfig.directory));
 
 app.use(routes);
