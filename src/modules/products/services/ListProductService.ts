3d80e06c5e47c8804066d12ee065ee241243756b
masprog2022 <masprog2022@gmail.com>
2022-05-31 11:21:03 +0100

    implementado a class RedisCache

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,14 +1,26 @@
 import { getCustomRepository } from "typeorm";
 import Product from "../typeorm/entities/Product";
 import { ProductsRepository } from "../typeorm/repositories/ProductsRepository";
+import RedisCache from "@shared/cache/RedisCache";
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductsRepository);
 
-    const product = productsRepository.find();
+    //const products = productsRepository.find();
+    const redisCache = new RedisCache();
 
-    return product;
+    let products = await redisCache.recover<Product[]>(
+      "api-vendas-PRODUCT_LIST"
+    );
+
+    if (!products) {
+      products = await productsRepository.find();
+
+      await redisCache.save("api-vendas-PRODUCT_LIST", products);
+    }
+
+    return products;
   }
 }
 
